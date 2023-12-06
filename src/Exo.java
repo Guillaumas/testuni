c109be990440a2d909babd17f21c02cc3b8284b2
Guillaumas <guillaume.gillet@ecoles-epsi.net>
2023-12-06 09:12:33 +0100

    lol

--- src/Exo.java
+++ src/Exo.java
@@ -1,1 +1,60 @@
-init
+import org.joda.time.DateTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exo {
+    public static void main(String[] args) {
+
+
+        ArrayList langues = new ArrayList();
+        langues.add("Français");
+        langues.add("Anglais");
+        DateTime dt = new DateTime();
+        int hour = dt.getHourOfDay();
+        System.out.println("Dans quelle langues souhaitez vous parler ?" + langues);
+        Scanner langue = new Scanner(System.in);
+        String lang = langue.nextLine();
+        System.out.println("Vous avez choisi de parler " + lang);
+        if (lang.contentEquals("Français")) {
+            if (hour < 12) {
+                System.out.println("Bonjour");
+            } else {
+                System.out.println("Bonsoir");
+            }
+
+            Scanner input = new Scanner(System.in);
+            String str = input.nextLine();
+
+            if (str.contentEquals(new StringBuilder(str).reverse())) {
+                System.out.println("Bien dit !");
+            }
+
+            if (hour < 12) {
+                System.out.println("Au revoir !");
+            } else {
+                System.out.println("Bonne soirée");
+            }
+        } else {
+            if (hour < 12) {
+                System.out.println("Hello");
+            } else {
+                System.out.println("Good evening");
+            }
+
+            Scanner input = new Scanner(System.in);
+            String str = input.nextLine();
+
+            if (str.contentEquals(new StringBuilder(str).reverse())) {
+                System.out.println("Well said !");
+            }
+
+            if (hour < 12) {
+                System.out.println("Have a nice day");
+            } else {
+                System.out.println("Bye !");
+            }
+        }
+
+
+    }
+}
